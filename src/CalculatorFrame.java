83ea8af7453e738cb982ab212555ba0abcf3ac3a
Kxxle <64878567+Kxxle@users.noreply.github.com>
2025-11-10 21:20:25 +0100

    define calculator button layout with scientific functions

--- src/CalculatorFrame.java
+++ src/CalculatorFrame.java
@@ -10,4 +10,17 @@ public class CalculatorFrame extends JFrame implements ActionListener {
     private char operator;
 
     private DecimalFormat df = new DecimalFormat("0.########");
+
+    //DEFINING BUTTONS
+    private final String[] buttonLabels = {
+        "7", "8", "9", "/", "sin",
+        "4", "5", "6", "*", "cos",
+        "1", "2", "3", "-", "tan",
+        "0", ".", "=", "+", "√",
+        "log", "x^y", "π", "C"
+    };
+
+
+
+
 }
